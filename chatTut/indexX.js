a8467bf5a55b2216faef6f0392d4ec88319300ad
ZidanMusk <mahmoud.ismail.zidan@gmail.com>
2017-03-18 08:18:49 +0200

    room participants and room chat history supported

--- chatTut/indexX.js
+++ chatTut/indexX.js
@@ -12,62 +12,70 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-// Chatroom
-var numUsers = 0;
-//var chatroom;
+//TODO USE A DB (reddis)
+//NAIVE SOLUTION!
 var roomArr = [];
+var roomHist = [];
 
 io.on('connection', function (socket) {
   var addedUser = false;
-  //var chatroom;
-  
+  console.log("connected");
+    io.clients(function(error, clients){
+        if (error) throw error;
+        console.log(clients);
+    });
    // when the client emits 'add user', this listens and executes
   socket.on('addUser2Room', function (data) {
     if (addedUser) return;
-
-    //var chatroom = io.of('/' + data.roomname); //Initializes and retrieves the given Namespace by its pathname identifier nsp
-
     // we store the username in the socket session for this client and this room
     console.log(data.username, " is joining room ", data.roomname);
     socket.join(data.roomname);
-    /*if(data.roomname in roomArr){
-      roomArr[data.roomname]
-    }*/
-    io.clients(function(error, clients){
-          if (error) throw error;
-          console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
-    });
-    console.log(socket.rooms);
     socket.username = data.username;
     socket.roomname = data.roomname;
+    //user joins an existing room
+    if(data.roomname in roomArr){
+      roomArr[data.roomname] += 1;
+      //supply new user with room history
+      io.to(socket.id).emit('updateNewJoiner',roomHist[data.roomname]);
+
+    }
+    else{
+      //init this room history
+      roomHist[data.roomname] = [];
+      roomArr[data.roomname] = 1;
+
+    }
+    console.log(roomArr);
+      //console.log(roomArr[data.roomname]);
+
+    //console.log(socket.rooms);
+
     //console.log(socket.roomname);
-    ++numUsers;
+    //++numUsers;
     addedUser = true;
 
     io.in(data.roomname).emit('login', {
-      numUsers: numUsers
+      numUsers: roomArr[socket.roomname]
     });
 
     // sending to all clients in room except sender
     socket.to(data.roomname).emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsers: roomArr[socket.roomname]
     });
 
-
   });
-
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
+      roomHist[socket.roomname].push({username: socket.username,message: data});
     socket.to(socket.roomname).emit('new message', {
       username: socket.username,
       message: data
     });
   });
 
- 
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
@@ -85,13 +93,25 @@ io.on('connection', function (socket) {
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
+    console.log("disconnected");
+    io.clients(function(error, clients){
+        if (error) throw error;
+        console.log("MEx: ", clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+    });
     if (addedUser) {
-      --numUsers;
+        if(--roomArr[socket.roomname] == 0){
+          delete roomArr[socket.roomname];
+          delete roomHist[socket.roomname];
+        }
+        console.log("Rooms left : " , roomArr);
 
+        console.log(socket.username, " has left room ", socket.roomname);
       // echo globally that this client has left
+      socket.leave(socket.roomname);
+      //socket.leaveAll();
       socket.to(socket.roomname).emit('user left', {
         username: socket.username,
-        numUsers: numUsers
+        numUsers: roomArr[socket.roomname]
       });
     }
   });
