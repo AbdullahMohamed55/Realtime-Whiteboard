f769cdb20faae909c0e8b7bf4eb2b5c72f2eed7b
AdelAdham <adeladham95@gmail.com>
2017-04-15 20:22:16 +0200

    Solving Conflics

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var index_bucky = require('./routes/index_bucky');
-var users = require('./routes/users');
-var try_html = require('./routes/try_html');
 var whiteboard = require('./routes/whiteboard');
 
 var app = express();
@@ -27,12 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/index', index);
-app.use('/index_bucky', index_bucky);
-app.use('/users', users);
-app.use('/try_html', try_html);
-app.use('/', whiteboard);
+app.use('/whiteboard', whiteboard);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
