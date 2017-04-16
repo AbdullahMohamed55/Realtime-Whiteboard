efc7ad19340317f7b922f25bc321a824abc2586e
AdelAdham <adeladham95@gmail.com>
2017-04-16 15:28:36 +0200

    routing

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 var whiteboard = require('./routes/whiteboard');
 
@@ -24,9 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/users', users);
-app.use('/whiteboard', whiteboard);
+app.use('/', whiteboard);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
